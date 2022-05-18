8aee1ec69576318a2083e13b3f4c2482ec0a3f2c
Thamer Al-Gahtani <thgahtan1@gmail.com>
2022-05-18 15:38:54 +0300

    hw 01

---
+++ bin/hw_01.dart
@@ -0,0 +1,14 @@
+
+void main() {
+  var name= "Thamer Algahtani";
+  //this is my name 
+  print("enter your name");
+  const date_of_birth = "August 2nd 2001 ";
+  /*i was born on this date */
+  var mobile_num= "0531595099";
+  //i wont answer ussually asleep
+  print("my name is ${name} i was born on ${date_of_birth} and you can call me on this number ${mobile_num}");
+  print(mobile_num[4]);
+  print(name.length);
+  print(name);
+}
